ad3ea1876de6e8cb7b79c5819de6d8b2fc80cd81
kisleylity-cloud <kisleylity@gmail.com>
2026-03-25 13:49:40 -0300

    Implement Operador class extending Pessoa

--- src/Operador.java
+++ src/Operador.java
@@ -1,1 +1,24 @@
 
+public class Operador extends Pessoa implements Permissao {
+
+    public Operador(String nome, String CPF, String dataNascimento, String login, String senha) {
+        super(nome, CPF, dataNascimento, login, senha);
+    }
+
+    @Override
+    public boolean acessarSistema(String login, String senha) {
+        return getLogin().equals(login) && verificarSenha(senha);
+    }
+
+    @Override
+    public void exibirPermissoes() {
+        System.out.println("Operador tem acesso limitado às operações do sistema.");
+    }
+
+    @Override
+    public void exibirPessoa() {
+        System.out.println("Operador: " + getNome() +
+                " | CPF: " + getCPF() +
+                " | Data de Nascimento: " + getDataNascimento());
+    }
+}
